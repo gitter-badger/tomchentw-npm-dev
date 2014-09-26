ec74e8009ff669b6e47b77bfdb25245470371a66
tomchentw <developer@tomchentw.com>
2014-09-26 10:53:03 +0800

    fix(publish-2-npm): update git tag after commit amend

--- bin/publish-2-npm.sh
+++ bin/publish-2-npm.sh
@@ -17,11 +17,14 @@ npm version $new_version_semver -m "chore(package.json): bump version to %s"
 npm version
 
 echo "Bump version completed"
+
+new_version=$(./node_modules/.bin/lsc -e "require './package.json' .version |> console.log")
 if [[ ! -f $changelog_name ]]; then
   old_version=$(git log --pretty=format:%H | tail -1)
 fi
 ./node_modules/.bin/lsc ./lib/changelog.ls $changelog_name $old_version
 git add $changelog_name
 git commit --amend --no-edit
+git tag -fa v$new_version
 
 echo "Please run \`npm publish\` and \`git push --tags\` manually"
